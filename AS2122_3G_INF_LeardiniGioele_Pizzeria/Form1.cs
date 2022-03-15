723fda449e29bda45fbc89842a31ec773e1addf8
Ryzen <Ryzen@DESKTOP-HF98UPO>
2022-03-15 20:20:33 +0100

    Aggiungere i file di progetto.

---
+++ AS2122_3G_INF_LeardiniGioele_Pizzeria/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2122_3G_INF_LeardiniGioele_Pizzeria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
